c9ff7cfdc7695a8c51aae359a1ab95e93b0e33c8
Simon <simon_brandt@gmx.de>
2019-08-23 10:52:06 +0200

    ADD stripeButton component

---
+++ src/components/stripeButton.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react"
+
+const StripeButton = ({ sku_id, name }) => {
+  const [stripe, setStripe] = useState(undefined)
+
+  useEffect(() => {
+    setStripe(window.Stripe("pk_test_fJeBDpFjDQ3ijdBrBi23i3Dk00RcsVwxMV"))
+  }, [])
+
+  return (
+    <form
+      onSubmit={ event => {
+        console.log("pressed")
+        event.preventDefault()
+        stripe.redirectToCheckout({
+          items: [{ sku: sku_id, quantity: 1 }],
+          successUrl: "http://localhost:8000/success",
+          cancelUrl: "http://localhost:8000/canceled",
+        })
+          .then(function(result) {
+            if (result.error) {
+              var displayError = document.getElementById("error-message")
+              displayError.textContent = result.error.message
+            }
+          })
+      } }
+      action=""
+    >
+      <button type={ "submit" }>
+        Buy { name }
+      </button>
+    </form>
+  )
+}
+
+export default StripeButton

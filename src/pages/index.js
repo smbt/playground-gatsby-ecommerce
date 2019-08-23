d4e011ab30915188063556c41aeb5b83638b61e5
Simon <simon_brandt@gmx.de>
2019-08-23 10:55:30 +0200

    CHANGE index. DISPLAY only sku components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,57 +1,46 @@
-import React, { useState, useEffect } from "react"
+// Libraries
+import React from "react"
+import { graphql } from "gatsby"
+
+// Components
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Sku from "../components/sku"
 
-const Button = () => {
-  const [stripe, setStripe] = useState(undefined)
-
-  useEffect(() => {
-    setStripe(window.Stripe("pk_test_fJeBDpFjDQ3ijdBrBi23i3Dk00RcsVwxMV"))
-  }, [])
-
-  return (
-    <form
-      onSubmit={ event => {
-        console.log("pressed")
-        event.preventDefault()
-        stripe.redirectToCheckout({
-          items: [{ sku: "sku_Ffc4kEeW6KSGq4", quantity: 1 }],
-
-          // Do not rely on the redirect to the successUrl for fulfilling
-          // purchases, customers may not always reach the success_url after
-          // a successful payment.
-          // Instead use one of the strategies described in
-          // https://stripe.com/docs/payments/checkout/fulfillment
-          successUrl: "http://localhost:8000/success",
-          cancelUrl: "http://localhost:8000/canceled",
-        })
-          .then(function(result) {
-            if (result.error) {
-              // If `redirectToCheckout` fails due to a browser or network
-              // error, display the localized error message to your customer.
-              var displayError = document.getElementById("error-message")
-              displayError.textContent = result.error.message
+// Data
+export const allStripeSku = graphql`
+{
+  allStripeSku {
+    edges {
+      node {
+        id
+        currency
+        price
+        attributes {
+          name
+        }
+        image
+        localFiles {
+          childImageSharp {
+            fluid(maxWidth: 500) {
+            ...GatsbyImageSharpFluid_tracedSVG
             }
-          })
-      } }
-      action=""
-    >
-      <button type={ "submit" }>
-        Buy hoddie
-      </button>
-    </form>
-  )
-
+          }
+        }
+      }
+    }
+  }
 }
+`
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
 
   return <Layout>
     <SEO title="Home"/>
-    <h1>Hi people</h1>
     <p>Welcome to playground-gatsby-ecommerce.</p>
-    <Button/>
-
+    {
+      data.allStripeSku.edges.map(sku => <Sku sku={ sku.node }/>)
+    }
   </Layout>
 
 }

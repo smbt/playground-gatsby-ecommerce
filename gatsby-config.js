b83a8e3146e87c0cd23be1add900be6b76d45b71
Simon <simon_brandt@gmx.de>
2019-08-24 21:31:50 +0200

    UNINSTALL gatsby-plugin-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,13 +39,6 @@ module.exports = {
             },
         },
         {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `images`,
-                path: `${__dirname}/src/images`,
-            },
-        },
-        {
             resolve: `gatsby-plugin-manifest`,
             options: {
                 name: `gatsby-starter-default`,
@@ -54,7 +47,7 @@ module.exports = {
                 background_color: `rebeccapurple`,
                 theme_color: `rebeccapurple`,
                 display: `minimal-ui`,
-                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+                // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
             },
         },
         {

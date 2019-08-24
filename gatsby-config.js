eb5b6df477f86d65a40ae47818648e7db2fd1b9c
Simon <simon_brandt@gmx.de>
2019-08-24 19:35:16 +0200

    ADD typescript configurations

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,45 +1,69 @@
 require(`dotenv`).config({
-  path: `.env.development`,
-});
+    path: `.env.development`,
+})
 
 
 module.exports = {
-  siteMetadata: {
-    title: `playground-gatsby-ecommerce`,
-    description: `-`,
-    author: `Simon Brandt`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-stripe`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${ __dirname }/src/images`,
-      },
+    siteMetadata: {
+        title: `playground-gatsby-ecommerce`,
+        description: `-`,
+        author: `Simon Brandt`,
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `rebeccapurple`,
-        theme_color: `rebeccapurple`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: `gatsby-source-stripe`,
-      options: {
-        objects: [`Product`, `Sku`],
-        secretKey: process.env.STRIPE_SECRET_KEY_READ_PRODUCTS_AND_SKUS,
-        downloadFiles: true,
-      },
-    },
-  ],
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        `gatsby-plugin-stripe`,
+        {
+            resolve: `gatsby-plugin-typescript`,
+            options: {
+                'target': 'es5',
+                'lib': [
+                    'dom',
+                    'dom.iterable',
+                    'esnext',
+                ],
+                'allowJs': true,
+                'skipLibCheck': true,
+                'esModuleInterop': true,
+                'allowSyntheticDefaultImports': true,
+                'strict': true,
+                'forceConsistentCasingInFileNames': true,
+                'module': 'esnext',
+                'moduleResolution': 'node',
+                'resolveJsonModule': true,
+                'isolatedModules': true,
+                'noEmit': true,
+                'jsx': 'preserve',
+                'baseUrl': 'src',
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `rebeccapurple`,
+                theme_color: `rebeccapurple`,
+                display: `minimal-ui`,
+                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+            },
+        },
+        {
+            resolve: `gatsby-source-stripe`,
+            options: {
+                objects: [`Product`, `Sku`],
+                secretKey: process.env.STRIPE_SECRET_KEY_READ_PRODUCTS_AND_SKUS,
+                downloadFiles: true,
+            },
+        },
+    ],
 }

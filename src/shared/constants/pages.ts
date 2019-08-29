7820bb9ba8f894adc4bd795953046e62a3e32780
Simon <simon_brandt@gmx.de>
2019-08-29 15:51:08 +0200

    ADD pages.ts

---
+++ src/shared/constants/pages.ts
@@ -0,0 +1,14 @@
+export default [
+    {
+        title: 'home',
+        path: '/',
+    },
+    {
+        title: 'cart',
+        path: '/cart',
+    },
+    {
+        title: 'contact',
+        path: '/contact',
+    },
+]

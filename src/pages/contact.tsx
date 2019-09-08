a1fc1a2b85e493d328841555bf2c08e5c8c58e43
Simon <simon_brandt@gmx.de>
2019-09-08 12:18:06 +0200

    CHANGE pages/contact: USE formspree

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -8,16 +8,14 @@ import Layout from 'components/Layout'
 import SEO from 'components/Seo'
 
 const ContactPage = () => {
-    const [name, setName] = useState<string | null>(null)
-    const [email, setEmail] = useState<string | null>(null)
-    const [message, setMessage] = useState<string | null>(null)
+    const [name, setName] = useState<string>('')
+    const [email, setEmail] = useState<string>('')
+    const [message, setMessage] = useState<string>('')
 
     const handleSubmit = () => {
-        console.warn('Sending must be implemented')
-        navigate('/contactSuccess')
+        // console.warn('Sending must be implemented')
+        // navigate('/contactSuccess')
     }
-
-    useEffect(() => console.log(name))
 
     return (
         <Layout>
@@ -26,37 +24,48 @@ const ContactPage = () => {
                 Contact us
             </Typography>
             <Container maxWidth={'sm'}>
-                <TextField
-                    label={'Name'}
-                    value={name}
-                    onChange={e => setName(e.target.value)}
-                    fullWidth
-                    margin={'normal'}
-                />
-                <br />
-                <TextField
-                    label={'E-mail'}
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                    fullWidth
-                    margin={'normal'}
-                />
-                <br />
-                <TextField
-                    label={'Message'}
-                    value={message}
-                    onChange={e => setMessage(e.target.value)}
-                    multiline
-                    rows={5}
-                    fullWidth
-                    margin={'normal'}
-                />
-                <Button onClick={handleSubmit} variant={'outlined'}>
-                    send
-                </Button>
+                <form
+                    action={'https://formspree.io/simon_foster116@yahoo.de'}
+                    method={'POST'}
+                >
+                    <TextField
+                        label={'Name'}
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                        fullWidth
+                        margin={'normal'}
+                    />
+                    <br />
+                    <TextField
+                        label={'E-mail'}
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        fullWidth
+                        margin={'normal'}
+                    />
+                    <br />
+                    <TextField
+                        label={'Message'}
+                        value={message}
+                        onChange={e => setMessage(e.target.value)}
+                        multiline
+                        rows={5}
+                        fullWidth
+                        margin={'normal'}
+                    />
+                    <Button
+                        type="submit"
+                        onClick={handleSubmit}
+                        variant={'outlined'}
+                    >
+                        send
+                    </Button>
+                </form>
             </Container>
         </Layout>
     )
 }
 
 export default ContactPage
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quibusdam voluptatum a nisi commodi, consequuntur inventore reprehenderit officiis totam harum maxime! Suscipit officiis beatae esse vitae, quisquam asperiores accusantium? Mollitia, laudantium eaque! Laborum hic dignissimos consequatur nobis placeat fuga cumque aut. Saepe perspiciatis debitis distinctio officiis! Est, libero soluta officia quasi tenetur, in, nihil corrupti repudiandae accusamus nemo omnis quia iure praesentium non esse dolores. Dicta id eveniet vitae perferendis dolores! Ullam, nemo. Inventore odit nulla molestias ipsa consequuntur aspernatur adipisci itaque. Nam, tenetur modi tempora beatae nulla animi perferendis adipisci quos, laudantium veniam voluptatum architecto debitis, commodi facilis quam!

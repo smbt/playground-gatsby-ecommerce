c2b977da4515b3ebaa6c98dd3ad0faee08fb6c92
Simon <simon_brandt@gmx.de>
2019-09-08 12:58:05 +0200

    INSTALL and CONFIG gatsby-plugin-playground

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
         `gatsby-plugin-react-helmet`,
         `gatsby-plugin-sharp`,
         `gatsby-plugin-stripe`,
+        `gatsby-plugin-playground`,
         `gatsby-transformer-sharp`,
         `gatsby-transformer-remark`,
         {

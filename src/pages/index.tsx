fa84da1a183b063e5ebac9debad31cab8351378e
Simon <simon_brandt@gmx.de>
2020-02-20 17:24:58 +0100

    Teständerung für Präsentation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -52,7 +52,7 @@ interface Props {
 }
 
 export default (props: Props) => {
-    const [movieResults, setMovieResults] = useState<Movie[]>([])
+        const [movieResults, setMovieResults] = useState<Movie[]>([])
     const [search, setSearch] = useState<string>('')
 
     useEffect(() => {
@@ -91,6 +91,9 @@ export default (props: Props) => {
                 style={{ width: '100%', height: 400 }}
             />
             <Layout>
+
+                Teständerung für Präsentation
+
                 <h1>eCommerce Prototype</h1>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem ducimus harum ipsam magnam qui soluta
                 voluptates. Distinctio dolorem harum, modi nobis optio repellat sequi? Accusamus adipisci aliquam
